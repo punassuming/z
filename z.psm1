41b843de0f1fa0ff14d2ef0a1e27b671897326ac
Vince Panuccio <panuccio.vince@gmail.com>
2014-05-29 22:59:49 +1000

    UNC paths are the exception to the Path property.

--- z.psm1
+++ z.psm1
@@ -280,10 +280,10 @@ function Save-CdCommandHistory() {
 }
 
 function Get-FormattedLocation() {
-	if ((Get-Location).Provider.ImplementingType.Name -eq 'RegistryProvider') {
-		Get-Location | select -ExpandProperty Path # The registry provider does return a path which z understands. In other words, I'm too lazy.
+	if ((Get-Location).Provider.ImplementingType.Name -eq 'FileSystemProvider' -and (Get-Location).Path.Contains('FileSystem::\\')) {
+		Get-Location | select -ExpandProperty ProviderPath # The registry provider does return a path which z understands. In other words, I'm too lazy.
 	} else {
-		Get-Location | select -ExpandProperty ProviderPath
+		Get-Location | select -ExpandProperty Path
 	}
 }
 
